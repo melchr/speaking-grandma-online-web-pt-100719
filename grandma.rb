4e884d855a598a9ad3eb6fed08ccf869ad6606e7
Melissa Christman <melchrstmn@gmail.com>
2019-10-08 12:22:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string == string.downcase
+  if string.downcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
