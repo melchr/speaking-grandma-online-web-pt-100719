14f237d8cf2f73f0c9c3889b8c0a84b9b54c11d5
Melissa Christman <melchrstmn@gmail.com>
2019-10-08 12:25:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,12 @@
+def speak_to_grandma(string)
+  if string == string.downcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "NO, NOT SINCE 1938!"
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -11,12 +20,3 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-
-def speak_to_grandma(string)
-  if string == string.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else
-    puts "NO, NOT SINCE 1938!"
-end
