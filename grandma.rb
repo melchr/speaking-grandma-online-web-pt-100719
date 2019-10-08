cd3947bdba9fc4cfa1e397514affaee47a7e88da
Melissa Christman <melchrstmn@gmail.com>
2019-10-08 12:31:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,11 @@
-
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+end
 
 # Write a speak_to_grandma method.
 
